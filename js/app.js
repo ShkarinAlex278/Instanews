1ead8871f900d35dad18c589d39bf50dbc513c8d
AlexShkarin <shkarinalexander278@gmail.com>
2020-02-10 14:14:20 -0500

    clear

--- js/app.js
+++ js/app.js
@@ -1,40 +1,23 @@
 $(function() {
-  // let $name = "michael jackson";
-  $.ajax({
-    method: "GET",
-    url:
-      "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8"
-    // "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=books"
-    //DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8
-  }).done(function(data) {
-    //  Start check Array
-    var startIndex = 0,
-      section = data.results[startIndex].section,
-      selectArray = [];
+  $("select").on("change", function() {
+    $(".display").empty();
 
-    while (startIndex < data.results.length) {
-      selectArray.push(data.results[startIndex].section);
-      startIndex++;
-    }
-    // My new loop
-    var newArray = [];
-    $.each(selectArray, function(i, name) {
-      if ($.inArray(name, newArray) === -1) newArray.push(name);
+    let $selection = $(this).val();
+    $.getJSON(
+      `https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8`
+    ).done(function(data) {
+      let counter = 0;
+      $.each(data.results, function(key, value) {
+        if (value.multimedia.length !== 0 && counter < 12) {
+          counter++;
+
+          $(".display").append(
+            `<figure><a href ="${value.url}target="_blank"><img src="${value.multimedia[4].url} "/><p>${value.abstract}</p></a></figure>`
+          );
+
+          return counter !== 12;
+        }
+      });
     });
-
-    $.each(newArray, function(i, name) {
-      $("#myList").append(`<option>${name}</option>`);
-    });
-    // End check array
-  });
-
-  let myCatecory;
-
-  $(".choose").change(function() {
-    myCatecory = $("#myList option:selected").text();
-    console.log("1 - ", myCatecory);
-
-    $("#box").append(myCatecory);
-    //return myCatecory;
   });
 });
