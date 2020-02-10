a0596f0cbc6380a461d2bdb459b44cd3801626a1
AlexShkarin <shkarinalexander278@gmail.com>
2020-02-10 15:01:06 -0500

    chore: sc file done..

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@ $(function() {
   $("select").on("change", function() {
     $(".display").empty();
 
+    //CSS style add
+    $(".display").css("grid-template-columns:", "0px", "0px", "0px");
+    $(".display").css("grid-template-rows: ", "0px", "0px", "0px", "0px");
+    //grid-template-rows: 350px 350px 350px 350px;
+    // CSS style end
+
     let $selection = $(this).val();
     $.getJSON(
       `https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8`
     ).done(function(data) {
+      // CSSstyle chenging --
+      $(".display").css("display", "grid");
+
+      // End CSS style --
       let counter = 0;
       $.each(data.results, function(key, value) {
         if (value.multimedia.length !== 0 && counter < 12) {
@@ -14,7 +24,7 @@ $(function() {
           $(".display").append(
             `<figure><a href ="${value.url}target="_blank"><img src="${value.multimedia[4].url} "/><p>${value.abstract}</p></a></figure>`
           );
-
+          // -- for git
           return counter !== 12;
         }
       });
