67debc55c941def0f885accad03e9d117983ed01
AlexShkarin <shkarinalexander278@gmail.com>
2020-02-11 16:02:04 -0500

    chore: css style for mobile..

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ $(function() {
     $(".myList").empty();
 
     //CSS style add
-    $(".myList").css("grid-template-columns:", "0px", "0px", "0px");
-    $(".myList").css("grid-template-rows: ", "0px", "0px", "0px", "0px");
-    //grid-template-rows: 350px 350px 350px 350px;
+    // $(".myList").css("grid-template-columns:", "0px", "0px", "0px");
+    // $(".myList").css("grid-template-rows: ", "0px", "0px", "0px", "0px");
+    // //grid-template-rows: 350px 350px 350px 350px;
     // CSS style end
 
     let $selection = $(this).val();
@@ -13,7 +13,7 @@ $(function() {
       `https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8`
     ).done(function(data) {
       // CSSstyle chenging --
-      $(".myList").css("display", "grid");
+      //$(".myList").css("display", "grid");
 
       // End CSS style --
       let counter = 0;
@@ -22,16 +22,16 @@ $(function() {
           counter++;
 
           $(".myList").append(
-            `<figure><a href ="${value.url}target="_blank"><img src="${value.multimedia[4].url} "/><p>${value.abstract}</p></a></figure>`
+            `<figure><a href ="${value.url}target="_blank"><img src="${value.multimedia[0].url} "/><p>${value.abstract}</p></a></figure>`
           );
           // -- for git
           return counter !== 12;
         }
       });
 
-      $(".items").css("width", "50%");
-      $(".items").css("height", "200px");
-//      $(".items").css("padding", "20px");
+      // $(".items").css("width", "50%");
+      // $(".items").css("height", "250px");
+      // $(".items").css("padding", "50px");
     });
   });
 });
