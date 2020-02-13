0ecece9675d05929e3534ebb7693bbfefa45ac92
AlexShkarin <shkarinalexander278@gmail.com>
2020-02-13 16:09:57 -0500

    chore: css style for tablet..

--- js/app.js
+++ js/app.js
@@ -2,20 +2,10 @@ $(function() {
   $("select").on("change", function() {
     $(".myList").empty();
 
-    //CSS style add
-    // $(".myList").css("grid-template-columns:", "0px", "0px", "0px");
-    // $(".myList").css("grid-template-rows: ", "0px", "0px", "0px", "0px");
-    // //grid-template-rows: 350px 350px 350px 350px;
-    // CSS style end
-
     let $selection = $(this).val();
     $.getJSON(
       `https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=DfA9QaZMuWxAfR4UZ4SltsgQUr8SyMk8`
     ).done(function(data) {
-      // CSSstyle chenging --
-      //$(".myList").css("display", "grid");
-
-      // End CSS style --
       let counter = 0;
       $.each(data.results, function(key, value) {
         if (value.multimedia.length !== 0 && counter < 12) {
@@ -28,10 +18,6 @@ $(function() {
           return counter !== 12;
         }
       });
-
-      // $(".items").css("width", "50%");
-      // $(".items").css("height", "250px");
-      // $(".items").css("padding", "50px");
     });
   });
 });
